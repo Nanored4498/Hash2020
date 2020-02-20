ef73e479887fd7b8abee61354c900b3831e3a70f
Nanored4498 <nanored.mc@gmail.com>
2020-02-20 18:59:50 +0100

    input

--- yoann.cpp
+++ yoann.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,7 +7,21 @@ int main() {
 	cout.tie(nullptr);
 	ios::sync_with_stdio(false);
 
-	
+	int B, L, D;
+	cin >> B >> L >> D;
+	vector<int> S(B);
+	vector<int> N(L), T(L), M(L);
+	vector<vector<int>> books(L), librairies(B);
+	for(int i = 0; i < B; i++) cin >> S[i];
+	for(int i = 0; i < L; i++) {
+		cin >> N[i] >> T[i] >> M[i];
+		int b;
+		for(int j = 0; j < N[i]; j++) {
+			cin >> b;
+			librairies[b].push_back(i);
+			books[i].push_back(b);
+		}
+	}
 
 	return 0;
 }
