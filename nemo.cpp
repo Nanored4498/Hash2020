260a19c1e022a707d36fb00e4382a674e6dabc58
Nemo Fournier <nemo.fournier@ens-lyon.org>
2020-02-20 21:22:54 +0100

    nemo's a little bit less stupid GLOUTON

--- nemo.cpp
+++ nemo.cpp
@@ -25,34 +25,57 @@ int main() {
         }
     }
 
-    vector<int> score_each_lib(L, 0);
+    vector<double> score_each_lib(L, 0.);
 
+    cerr << "yo" << endl;
     for(int i = 0; i < L; i++) {
         vector<int> score;
-        for(int book : books[i]) {
-            score.push_back(S[book]);
+        for(int b = 0; b < N[i]; b++) {
+            score.push_back(S[b]);
         }
         sort(score.begin(), score.end());
         for(int day = 0; day < D - T[i]; day++) {
-            for(int curr_book = M[i]-1; curr_book >= 0; curr_book--) {
-                score_each_lib[i] += score[day*M[i] + curr_book];
+            for(int curr_book = 0; curr_book < M[i]; curr_book++) {
+                if(day*M[i]+curr_book > N[i]-1) break;
+
+                score_each_lib[i] += score[N[i] - (day*M[i] + curr_book) - 1];
             }
+
+            if(day*M[i] > N[i]-1) break;
         }
+        score_each_lib[i] *= (double)(M[i]*M[i]*N[i])/((double)(T[i]*T[i]*T[i]*T[i]));
     }
 
-    vector<pair<int,int>> lib_orders;
+    vector<pair<double,int>> lib_orders;
     for(int i = 0; i < L; i++) {
         lib_orders.push_back(make_pair(score_each_lib[i],i));
     }
 
     sort(lib_orders.begin(), lib_orders.end());
 
-    // register everyone...
-    cout << L << endl;
+    int curr_day = 0;
+
+    int nb = 0;
 
     for(int i = L-1; i >= 0; i--) {
         int current_lib = lib_orders[i].second;
+        curr_day += T[current_lib];
+        if(curr_day>= D) break;
+        nb++;
+    }
+    // register everyone...
+    cout << nb << endl;
+    curr_day = 0;
+
+    vector<bool> sent(B, false);
+
+    for(int i = L-1; i >= 0; i--) {
+        int current_lib = lib_orders[i].second;
+        curr_day += T[current_lib];
+        if(curr_day>= D) break;
+        
         cout << current_lib << " " << N[current_lib] << endl;
+
         //sort books
 
         vector<pair<int,int> > book_order;
@@ -61,11 +84,24 @@ int main() {
         }
 
         sort(book_order.begin(), book_order.end());
+        int count_new_sent = 0;
+        vector<int> discarded;
 
         for(int book = N[current_lib]-1; book >=0; book--) {
+            while(sent[book_order[book].second] && book>=0) {
+                discarded.push_back(book_order[book].second);
+                book--;
+            }
+            if(book < 0) break;
+            sent[book_order[book].second] = true;
+            count_new_sent++;
             if(book != N[current_lib]-1) cout << " ";
             cout << book_order[book].second;
         }
+        for(int b : discarded) {
+            cout << " " << b;
+        }
+
         cout << endl;
     }
 
